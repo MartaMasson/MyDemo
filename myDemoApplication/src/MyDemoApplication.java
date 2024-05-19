cc632c25c9cba5931939d5cd868c943acfafce92
Marta Masson <marta.masson@gmail.com>
2024-05-19 19:35:26 -0300

    refactored the hello world sentence

--- myDemoApplication/src/MyDemoApplication.java
+++ myDemoApplication/src/MyDemoApplication.java
@@ -2,7 +2,7 @@ public class MyDemoApplication {
 
     public static void main(String[] args) {
         int iFatorial = 5;
-        System.out.println("Hello World. Let´s calculate the fatorial of " + iFatorial + "!");
+        System.out.println("Let´s calculate the fatorial of " + iFatorial + "!");
         Fatorial fatorial = new Fatorial();
         System.out.println("\n Final Result of fatorial " + iFatorial + ": " + fatorial.calculate(iFatorial));
     }
